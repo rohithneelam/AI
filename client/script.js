2c9c36ca2823a383b26f46b7626edd62b16c927b
rohithneelam <114081582+rohithneelam@users.noreply.github.com>
2023-01-13 17:51:21 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://bhargavi-group-ai.onrender.com', {
+    const response = await fetch('https://rohith-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
